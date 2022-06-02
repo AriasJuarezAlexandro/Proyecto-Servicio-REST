3277f4236e46f6392d9473490c7f443701a9a800
AriasJuarezAlexandro <ariasjuarezalex@gmail.com>
2022-06-02 18:46:50 -0500

    inicio

---
+++ src/main/java/com/proyecto/servidor/ProyectoServicioRestApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoServicioRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoServicioRestApplication.class, args);
+	}
+
+}
